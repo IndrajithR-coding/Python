db1460774d38568ea933d7e6f952f26e3880b35f
IndrajithR-coding <indrajithr2008@gmail.com>
2026-08-01 11:07:17 +0530

    Add basic program
    Added a comment to clarify the purpose of the script.

---
+++ Temperature scale/Degree Fahrenheit to Celsius.py
@@ -0,0 +1,4 @@
+#Python prgram to convert temperature in Fahrenheit into degree Celsius
+f=eval(input("Enter the temperature in Fahrenheit:"))
+c=(f-32)*5/9
+print("Temperature in degree Celsius:",c)

8c8ffd98ae3e33df5e2fd0d50ff37b16d3dc25f2
IndrajithR-coding <indrajithr2008@gmail.com>
2026-08-01 11:29:35 +0530

    Add expression calculation program in Python
    This program prompts the user for values of x, y, and z, and calculates a specific mathematical expression using these variables.

---
+++ Expression Calculation/Obtaining variable value, calculating expression.py
@@ -0,0 +1,6 @@
+#Python program to obtain x,y,z from user and calculate expression
+x=eval(input("Enter x="))
+y=eval(input("Enter y="))
+z=eval(input("Enter z="))
+a=4*x**4+3*y**3+9*z+6*3.14
+print("Value of the expression",exp,"is", a)

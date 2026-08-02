4c1a39eda922520edd9ad3b312ca518005c07225
IndrajithR-coding <indrajithr2008@gmail.com>
2026-08-02 17:23:08 +0530

    Enhance BMI calculation with classification output
    Added BMI classification based on calculated value.

--- BMI/BMI calculation.py
+++ BMI/BMI calculation.py
@@ -1,5 +1,13 @@
 #Python program to calculate BMI of a person
 kg=eval(input("Enter the weight:"))
 m=eval(input("Enter the height(in m):"))
-bmi=kg/(m*m)
-print("The BMI of the person=",bmi)
+bmi=kg/(m***2)
+print("BMI=",bmi)
+if (bmi<18.5):
+  print("Underweight!!")
+elif bmi>=18.5 and bmi<=24.9:
+  print("Normal! Good job!!")
+elif bmi>=25 and bmi<=29.9:
+  print("Overweight")
+elif bmi>=30:
+  print(!!Obese!!")

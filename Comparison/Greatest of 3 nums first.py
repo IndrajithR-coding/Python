d6339c25384e4681ca3e65baa79c9122b8a4d48b
IndrajithR-coding <indrajithr2008@gmail.com>
2026-08-03 21:29:24 +0530

    Refactor greatest number logic with user choice

--- Comparison/Greatest of 3 nums first.py
+++ Comparison/Greatest of 3 nums first.py
@@ -1,13 +1,25 @@
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 c=int(input("Enter the third number:"))
-if a>b:
-  if a>c:
-    print(a," is the greatest number.")
+n=input("Give a for first code, b for second code:")
+if n==a:
+  if a>b:
+    if a>c:
+      print(a," is the greatest number.")
+    else:
+      print(c," c is the greatest number.")
   else:
-    print(c," c is the greatest number.")
-else:
-  if b>c:
+    if b>c:
+      print(b," is the greatest number.")
+    else:
+      print(c," is the greatest number.")
+elif n==b:
+  if a>b:
+    if a>c:
+      print(a," is the greatest number.")
+    else:
+      print(c," is the greatest number.")
+  elif b>c:
     print(b," is the greatest number.")
   else:
     print(c," is the greatest number.")
